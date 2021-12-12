da31e8a36a7e45e7d7c14898f7366cd2830bad82
clm1010 <limin_chu@163.com>
2021-12-13 01:53:06 +0800

    fix: 修改目录结构

---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon></el-icon>
+        </span>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+// 导入的组件可以直接使用
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
