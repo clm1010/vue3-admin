9f5e7a46c9d9afe62cd82b0abb89edb0acded5d1
clm1010 <limin_chu@163.com>
2021-12-15 01:00:47 +0800

    feat: 开发SvgIcon

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const port = process.env.port || process.env.npm_config_port || 8080
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    // 配置反向代理
+    proxy: {
+      // 当地址中有/api的时候会触发代理机制
+      '/api': {
+        // 要代理的服务器地址  这里不用写 api
+        target: 'https://api.imooc-admin.lgdsunday.club/',
+        changeOrigin: true // 是否跨域
+      }
+    }
+  }
+}
