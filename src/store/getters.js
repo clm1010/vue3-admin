48ef87a7b5f08ef03a6ec49195dc8847714c5238
clm1010 <limin_chu@163.com>
2021-12-29 16:29:23 +0800

    feat: 渲染用户头像菜单

--- src/store/getters.js
+++ src/store/getters.js
@@ -8,7 +8,8 @@ const getters = {
    */
   hasUserInfo: state => {
     return JSON.stringify(!state.user.userInfo) !== '{}'
-  }
+  },
+  userInfo: state => state.user.userInfo
 }
 
 export default getters
