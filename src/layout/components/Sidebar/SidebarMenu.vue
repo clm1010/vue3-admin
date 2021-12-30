51372fdef7c4bca323db5913a945d566e3a6a0f5
clm1010 <limin_chu@163.com>
2021-12-30 15:42:11 +0800

    feat: 创建页面组件，使用临时 menu 菜单

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-menu
+    :uniqueOpened="true"
+    background-color="#545c64"
+    text-color="#FFF"
+    active-text-color="#FFD04B"
+  >
+    <!-- 子集 menu -->
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><Menu /></el-icon>
+        <span>导航一</span>
+      </template>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+    </el-sub-menu>
+    <!-- 具体菜单 -->
+    <el-menu-item index="2">
+      <el-icon><location /></el-icon>
+      <template #title>导航4</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
