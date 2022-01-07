348eb4fc7298e5171cf0db34f5468a05b5d7f70c
clm1010 <limin_chu@163.com>
2022-01-07 17:26:29 +0800

    feat: sidebarHeader处理（部分功能）

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="">
-    <h1 :style="{color: $store.getters.cssVar.menuText}">占位</h1>
+    <!-- <h1 :style="{color: $store.getters.cssVar.menuText}">占位</h1> -->
+    <div class="logo-container">
+      <el-avatar
+        :size="44"
+        :src="avatarUrl"
+        :fit="`cover`"
+        shape="square"
+        @error="errorHandler"
+      >
+        <img :src="failedUrl" />
+      </el-avatar>
+      <h1 class="logo-title" v-if="$store.getters.sidebarOpened">Vue3-Admin</h1>
+    </div>
     <el-scrollbar>
       <sidebar-menu></sidebar-menu>
     </el-scrollbar>
@@ -8,7 +20,30 @@
 </template>
 
 <script setup>
+import logo from '@/assets/iron-man.svg'
+import imgFailed from '@/assets/img-failed.png'
 import SidebarMenu from './SidebarMenu'
+
+const failedUrl = imgFailed
+const avatarUrl = logo
+const errorHandler = () => true
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/variables.scss';
+.logo-container {
+  height: 44px;
+  padding: 10px 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo-title {
+    margin-left: 10px;
+    color: $logoColor;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
+</style>
