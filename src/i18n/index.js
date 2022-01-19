aba197b2cd1663f5c8a7b7eb46ff11ce67892753
clm1010 <limin_chu@163.com>
2022-01-19 15:53:41 +0800

    feat: 国际化缓存处理

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,8 @@
 import { createI18n } from 'vue-i18n'
 import zhLocale from './lang/zh'
 import enLocale from './lang/en'
+import store from '@/store'
+
 const messages = {
   en: {
     msg: {
@@ -14,14 +16,20 @@ const messages = {
   }
 }
 
-const locale = 'zh'
+/**
+ * @description 获取语言
+ * @returns 返回当前 lang
+ */
+function getLanguage() {
+  return store && store.getters && store.getters.language
+}
 
 const i18n = createI18n({
   // 使用 composition API
   legacy: false,
   // 全局使用 t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
