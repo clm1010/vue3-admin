21252162db654e83770a70505f33d3bc88a34928
clm1010 <limin_chu@163.com>
2022-02-18 14:30:23 +0800

    fix: 剩余问题处理（完成)

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -32,34 +32,39 @@ import { useRouter } from 'vue-router'
 import Fuse from 'fuse.js'
 import { filterRouters } from '@/utils/route'
 import { generateRoutes } from './FuseData'
+import { watchSwitchLang } from '@/utils/i18n'
 
 // 数据源
 const router = useRouter()
-const searchPool = computed(() => {
+let searchPool = computed(() => {
   const routes = filterRouters(router.getRoutes())
   return generateRoutes(routes)
 })
 
 // 搜索库相关
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进行排序
-  shouldSort: true,
-  // 匹配长度超过这个值的才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
-  // name：搜索的键
-  // weight：对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
-})
+let fuse
+const initFuse = (searchPool) => {
+  fuse = new Fuse(searchPool, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+initFuse(searchPool.value)
 
 // 控制 search 展示
 const isShow = ref(false)
@@ -67,7 +72,7 @@ const onShowClick = () => {
   isShow.value = !isShow.value
 }
 
-watch(isShow, val => {
+watch(isShow, (val) => {
   if (val) {
     headerSearchSelectRef.value.blur()
     document.body.addEventListener('click', onClose)
@@ -101,6 +106,14 @@ const querySearch = (query) => {
 const onSelectChange = (val) => {
   router.push(val.path)
 }
+
+watchSwitchLang(() => {
+  searchPool = computed(() => {
+    const routes = filterRouters(router.getRoutes())
+    return generateRoutes(routes)
+  })
+  initFuse(searchPool.value)
+})
 </script>
 
 <style lang="scss" scoped>
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,8 +1,23 @@
 import i18n from '@/i18n'
+import { watch } from 'vue'
+import store from '@/store'
 /**
  * @description 生成标题方法
  * @param {*} title 标题
  */
 export function generateTitle(title) {
   return i18n.global.t('msg.route.' + title)
+}
+
+/**
+ * @description 监听语言变化，同时执行 cb
+ * @param  {...any} cbs
+ */
+export function watchSwitchLang(...cbs) {
+  watch(
+    () => store.getters.language,
+    () => {
+      cbs.forEach(cb => cb(store.getters.language))
+    }
+  )
 }
