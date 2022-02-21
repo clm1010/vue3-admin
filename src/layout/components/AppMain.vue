e8b100d70b4865eeaf0d0838ac09a1fe9bf82163
clm1010 <limin_chu@163.com>
2022-02-21 17:29:16 +0800

    fix: tagsView (开发部分功能)

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -6,12 +6,44 @@
 
 <script setup>
 import { watch } from 'vue'
+import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
+import { isTags } from '@/utils/tags'
+import { generateTitle } from '@/utils/i18n'
 
+const getTitle = (route) => {
+  let title = ''
+  if (!route.meta) {
+    const pathArr = route.path.split('/')
+    title = pathArr[pathArr.length - 1]
+  } else {
+    title = generateTitle(route.meta.title)
+  }
+  return title
+}
+
+const store = useStore()
 const route = useRoute()
-watch(route, (to, from) => {
-  // 并不是所有的 路由都需要保存的
-})
+watch(
+  route,
+  (to, from) => {
+    // 并不是所有的 路由都需要保存的
+    if (!isTags(to.path)) return
+    const { fullPath, meta, name, params, path, query } = to
+    store.commit('app/addTagsViewList', {
+      fullPath,
+      meta,
+      name,
+      params,
+      path,
+      query,
+      title: getTitle(to)
+    })
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
 <style lang="scss" scoped>
