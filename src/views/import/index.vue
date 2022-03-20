4f762a0a54aaa1600b3eba22255c36e86364988f
clm1010 <limin_chu@163.com>
2022-03-21 00:46:45 +0800

    feat: 文件拖入之后的数据解析处理

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="">导入</div>
+  <upload-excel :onSuccess="onSuccess"></upload-excel>
 </template>
 
 <script setup>
+import UploadExcel from '@/components/UploadExcel'
+
+const onSuccess = excelData => {
+  console.log(excelData)
+}
 </script>
 
 <style lang="scss" scoped></style>
