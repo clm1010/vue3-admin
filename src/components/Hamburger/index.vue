6fd4cdee06fa398ea72a48c99c8d30379fcce4e0
clm1010 <limin_chu@163.com>
2022-11-23 13:49:58 +0800

    feat: 增加Hamburger文件夹

---
+++ src/components/Hamburger/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="guide-hamburger" class="hamburger-container" @click="toggleClick">
+    <!-- class="hamburger" -->
+    <svg-icon :icon="icon"></svg-icon>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const toggleClick = () => {
+  store.commit('app/triggerSidebarOpened')
+}
+
+const store = useStore()
+const icon = computed(() => {
+  return store.getters.sidebarOpened ? 'hamburger-opened' : 'hamburger-closed'
+})
+</script>
+
+<style lang="scss" scoped>
+.hamburger-container {
+  padding: 0 16px;
+  // .hamburger {
+  //   display: inline-block;
+  //   vertical-align: middle;
+  //   width: 20px;
+  //   height: 20px;
+  // }
+}
+</style>
